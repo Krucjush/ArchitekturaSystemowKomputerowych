0f8db28a126fab1d9ac87c3bda2a97b292bff1f3
Dawid Studziżba <Dawid.Studzizba@microsoft.wsei.edu.pl>
2022-03-10 15:46:27 +0100

    2

--- ArchitektoraSystemowKomputerowych/ArchitektoraSystemowKomputerowych/Program.cs
+++ ArchitektoraSystemowKomputerowych/ArchitektoraSystemowKomputerowych/Program.cs
@@ -6,7 +6,17 @@ namespace ArchitektoraSystemowKomputerowych
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string i;
+        }
+        static public string Check(string i)
+        {
+            long output;
+            if (i.Length == 2)
+            {
+                long.TryParse(i, System.Globalization.NumberStyles.HexNumber, null, out output);
+                return Convert.ToString(output);
+            }
+            return null;
         }
     }
 }
